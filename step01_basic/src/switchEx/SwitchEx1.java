551f15f17cdc75cb9daec7ba264900fb1ac443ff
sist <sist@DESKTOP-A8CR59P>
2018-11-17 16:44:42 +0900

    commit

--- step01_basic/src/switchEx/SwitchEx1.java
+++ step01_basic/src/switchEx/SwitchEx1.java
@@ -19,13 +19,54 @@ public class SwitchEx1 {
 		
 		//------------------------------------------------------
 		
-		char check='e';
-		if(check=='E' || check=='e' ) {
+		/*char check='e';
+		if(check=='E' || check=='e' ) 
 			System.out.println("Excellent");
-		} else if(check=='G' || check=='g') {
+		else if(check=='G' || check=='g') 
 			System.out.println("Good");
-		} else {
-			System.out.println("Bad");
+		else 
+			System.out.println("Bad");*/
+		
+		//------------------------------------------------------
+		
+		/*String position="Manager";
+		
+		switch(position) {
+		case "CEO" : 
+			System.out.println("Salary $20000");
+			break;
+		case "Manager" :
+			System.out.println("Salary $15000");
+			break;
+		case "Employee":
+			System.out.println("Salary $10000");
+			break;
+			
+		}*/
+		
+		int score=93;
+		char grade;
+		
+		switch(score/10) {
+		case 10:
+		case 9:	
+			grade = 'A';
+			break;
+		case 8:
+			grade = 'B';
+			break;
+		case 7:
+			grade ='C';
+			break;
+		case 6:
+			grade ='D';
+			break;
+		default:
+			grade = 'F';
+			break;
 		}
+		
+		System.out.println("나의 점수는 "+score+"점이고, "+grade+"학점 입니다.");
+		
 	}
 }
--- step01_basic/src/switchEx/SwitchEx2.java
+++ step01_basic/src/switchEx/SwitchEx2.java
@@ -1,7 +1,30 @@
 package switchEx;
+/*[문제]
+숫자를 입력하시오 : 5
 
+5는 홀수 입니다
+*/
 public class SwitchEx2 {
 	public static void main(String[] args) {
+		int num = 2;
 		
+		switch(num) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 9:
+			System.out.println(num+"는 홀수입니다.");
+			break;
+		case 2:
+		case 4:
+		case 6:
+		case 8:
+		case 10:
+			System.out.println(num+"는 짝수입니다.");
+			break;
+		default:
+			System.out.println("1-10의 범위 내에서 입력해주세요.");
+		}
 	}
 }
